ee1aa9e336eeec41fa9b1fc8d4e1dd4449f8e376
Celal <celal2344@gmail.com>
2024-10-13 15:19:09 +0300

    implemented infinite scrolling for character list page

---
+++ rick-and-morty/Modules/Characters-List/CharacterListView.swift
@@ -0,0 +1,44 @@
+//
+//  CharacterListView.swift
+//  rick-and-morty
+//
+//  Created by Celal on 7.10.2024.
+//
+
+import SwiftUI
+
+struct CharacterListView: View {
+    var viewModel = CharacterListViewModel.shared
+    
+    @State private var charList: CharacterListModel?
+    @State private var showError = false
+    var body: some View {
+        NavigationView {
+            VStack{
+                if let characters = charList?.results {
+                    List(characters) { character in
+                        CharacterView(character: character)
+                    }
+                } else if charList == nil {
+                    Text("No data available.")
+                        .font(.headline)
+                        .foregroundColor(.gray)
+                } else {
+                    ProgressView("Loading...")
+                }
+            }
+        }
+        .navigationTitle("Characters")
+        .task{
+            do{
+                charList = try await viewModel.getCharacters()
+            }catch{
+                showError.toggle()
+            }
+        }
+        .alert("Failed to fetch data", isPresented: $showError, actions: {})
+    }
+}
+#Preview {
+    CharacterListView()
+}
